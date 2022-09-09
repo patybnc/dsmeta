670ebc67dd4de64a2c3e3bbebc3247a5dc0b5ec5
Patricia Costa <patybnc@yahoo.com.br>
2022-09-09 20:26:06 -0300

    main window react

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -12,7 +12,7 @@ type Props = {
 function handleClick(id :number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            toast.info("SMS enciado com sucesso");
+            toast.info("SMS enviado com sucesso");
         });
 }
 
